63e7475d5ee7c4cfbe357a5c703621fc30abe405
Dtiger123 <88206528+Dtiger123@users.noreply.github.com>
2022-03-30 20:26:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,500)
+    canvas.position(560,150);
+    posenet = ml5.poseNet(video,modelloaded);
+    posenet.on('pose', gotposes);
+}
+function modelloaded(){
+    console.log("Model has been initialized");
+}
+function draw(){
+    background("blue")
+}
+function gotposes(results){
+if(results.length > 0){
+console.log(results);
+}
+}
